f11d51068e98b3efd0618c2d0d8ed48ae38ce724
乐 简 <2576950110@qq.com>
2022-07-28 15:21:30 +0800

    引用

---
+++ work/cutgraph/include/CutGraph.h
@@ -0,0 +1,9 @@
+#ifndef _CUT_GRAPH_H_
+#define _CUT_GRAPH_H_
+
+#include <iostream>
+
+using namespace std;
+
+
+#endif // !_CUT_GRAPH_H_
--- work/cutgraph/src/Viewer.cpp
+++ work/cutgraph/src/Viewer.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-
-using namespace std;
+#include "CutGraph.h"
 
 int main()
 {
